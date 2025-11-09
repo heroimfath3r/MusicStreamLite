b96b003473aa8eeb523916d42c46787a4a342bd2
John Doe <john.doe@contoso.com>
2025-11-09 17:33:45 -0500

    Fixing catalog-service

--- catalog-service/src/config/database.js
+++ catalog-service/src/config/database.js
@@ -80,7 +80,7 @@ export const deleteSong = async (fileName) => {
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
